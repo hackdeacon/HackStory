be8f0f004cccf5652268be50c94931f6abe4d58c
黑衣执事 <hackdeacon@hyzs.local>
2024-10-27 03:02:31 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "黑衣执事",
+  description: "hackdeacon",
+  head: [
+    ['link', {rel: 'icon', href: 'https://bu.dusays.com/2024/01/28/65b5fcbaa0b2c.png'}]
+  ],
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    // logo: 'https://img.icons8.com/stickers/500/valorant.png',
+    logo: 'https://bu.dusays.com/2024/01/28/65b5fcbaa0b2c.png',
+    footer: {
+      message: 'Hack Deacon',
+      copyright: 'Happy Myself with Heart'
+    },
+    nav: [
+      { text: '🏠', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'x', link: 'https://github.com/hackdeacon' }
+    ]
+  }
+})
---
+++ index.md
@@ -0,0 +1,44 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "黑胶故事"
+  text: "用心取悦自己"
+  tagline: hackdeacon.xyz
+  image: 
+    # src: 'https://img.icons8.com/stickers/500/hello-kitty.png'
+    src: 'https://bu.dusays.com/2023/10/19/653138be45ba5.png'
+    alt: Hello Kitty
+  actions:
+    - theme: brand
+      text: 作家
+      link: https://gpt.hackdeacon.xyz/
+    - theme: alt
+      text: GotoVlog
+      link: https://v.douyin.com/iS5jJ92A/
+
+features:
+  - icon: 📸
+    title: Photograph
+    details: 我会一直记录，<br>直到有人记录我。
+  - icon:
+      src: src/valorant.svg
+    title: Valorant
+    details: 黑衣执事<br>#60895
+  - icon: 🍕
+    title: X
+    details: 黄油小熊
+  - icon: 🍕
+    title: Y
+    details: 黄油小熊
+---
+<!-- ## Getting Started
+
+You can get started using VitePress right away using `npx`!
+
+```sh
+npm init
+npx vitepress init
+``` -->
+
